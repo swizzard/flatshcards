c1138860930d642fa259bbe4715efd8f8c40c1b3
Sam Raker <sam.raker@gmail.com>
2025-07-29 12:56:44 -0400

    lexicon generation

--- flatshcards_be/src/lexicons/record.rs
+++ flatshcards_be/src/lexicons/record.rs
@@ -2,20 +2,7 @@
 //!A collection of known record types.
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(tag = "$type")]
-pub enum KnownRecord {
-    #[serde(rename = "xyz.statusphere.status")]
-    LexiconsXyzStatusphereStatus(Box<crate::lexicons::xyz::statusphere::status::Record>),
-}
-impl From<crate::lexicons::xyz::statusphere::status::Record> for KnownRecord {
-    fn from(record: crate::lexicons::xyz::statusphere::status::Record) -> Self {
-        KnownRecord::LexiconsXyzStatusphereStatus(Box::new(record))
-    }
-}
-impl From<crate::lexicons::xyz::statusphere::status::RecordData> for KnownRecord {
-    fn from(record_data: crate::lexicons::xyz::statusphere::status::RecordData) -> Self {
-        KnownRecord::LexiconsXyzStatusphereStatus(Box::new(record_data.into()))
-    }
-}
+pub enum KnownRecord {}
 impl Into<atrium_api::types::Unknown> for KnownRecord {
     fn into(self) -> atrium_api::types::Unknown {
         atrium_api::types::TryIntoUnknown::try_into_unknown(&self).unwrap()
